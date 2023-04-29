42e84988f7be87dbbdc48c168c3ed375094e3e87
Danil Melkozerov <danmelk.dev@gmail.com>
2023-04-29 13:04:01 +0400

    feat: puppeter class wrapping

--- packages/src/article-module/articleCrawler.js
+++ packages/src/article-module/articleCrawler.js
@@ -1,8 +1,13 @@
 import puppeteer, { executablePath } from "puppeteer";
 import fs from "fs";
 
-function getBrowser() {
-  const args = [
+class HeadlessCrawler {
+  constructor() {
+    this.page = null;
+    this.browser = null;
+  }
+
+  args = [
     "--disable-background-timer-throttling",
     "--disable-backgrounding-occluded-windows",
     "--disable-ipc-flooding-protection",
@@ -12,19 +17,26 @@ function getBrowser() {
     "--disable-blink-features=AutomationControlled",
   ];
 
-  return puppeteer.launch({
-    executablePath: executablePath(),
-    args,
-    defaultViewport: null,
-    headless: true,
-  });
-}
+  techCrunchTopic = {
+    TechCrunchAI: 'https://techcrunch.com/category/artificial-intelligence/',
+    TechCrunchCrypto: 'https://techcrunch.com/category/cryptocurrency/',
+    TechCrunchSecurity: 'https://techcrunch.com/category/security/',
+    TechCrunchStartups: 'https://techcrunch.com/category/startups/',
+  }
 
-async function getArticle() {
-    const browser = await getBrowser();
-    const page = await browser.newPage();
-  
-    await page.goto("https://techcrunch.com/2023/04/27/muslims-come-into-the-frame-in-southeast-asias-fintech-boom/", {waitUntil: 'load'});
+  async launch() {
+    this.browser = await puppeteer.launch({
+      executablePath: executablePath(),
+      args,
+      defaultViewport: null,
+      headless: true,
+    });
+
+    this.page = await puppeteer.newPage();
+  }
+
+  async articleCrawler() {
+    await this.page.goto("https://techcrunch.com/2023/04/27/muslims-come-into-the-frame-in-southeast-asias-fintech-boom/", {waitUntil: 'load'});
     const html = await page.content();
   
     fs.writeFile('./content/article.html', html, (err) => {
@@ -33,5 +45,4 @@ async function getArticle() {
 
     await browser.close()
   };
-
-getArticle();
+}
