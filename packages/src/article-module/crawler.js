ad257659b20834a71ca3ab41450ce0d2bb4303bd
Danil Melkozerov <75336316+danmelk@users.noreply.github.com>
2023-05-02 00:54:09 +0400

    feat: puppeteer TechCrunch crawler (#5)
    * feat: puppeter class wrapping
    
    * build: small module renaming
    
    * feat: techcrunch crawler

---
+++ packages/src/article-module/crawler.js
@@ -0,0 +1,84 @@
+import puppeteer, { executablePath } from "puppeteer";
+import fs from "fs";
+
+class TechCrunchCrawler{
+  constructor() {
+    this.page = null;
+    this.browser = null;
+  }
+
+  /**
+   * The path of a folder where to save the HTML topic file
+   */
+  location = '../../content'
+  
+  args = [
+    "--disable-background-timer-throttling",
+    "--disable-backgrounding-occluded-windows",
+    "--disable-ipc-flooding-protection",
+    "--disable-renderer-backgrounding",
+    "--no-sandbox",
+    "--disable-setuid-sandbox",
+    "--disable-blink-features=AutomationControlled",
+  ];
+
+  topics = {
+    ai: '/category/artificial-intelligence/',
+    crypto: '/category/cryptocurrency/',
+    security: '/category/security/',
+    startups: '/category/startups/',
+  }
+
+  articleSelector = 'article h2 a[href]';
+
+  async open(path = "") {
+    return this.page.goto(`https://techcrunch.com${path}`, {waitUntil: 'load'})
+  }
+
+  /**
+   * Launches a new Chromium instance
+   * Returns two instances to operate the browser
+   */
+  async launch() {
+    this.browser = await puppeteer.launch({
+      executablePath: executablePath(),
+      args: this.args,
+      defaultViewport: null,
+      headless: true,
+    });
+
+    this.page = await this.browser.newPage();
+  }
+
+  /**
+   * Iterates over the topic list
+   */
+  async iterating() {
+    for (let topic in this.topics) {
+      await this.crawling(this.topics[topic], topic)
+    }
+  }
+
+  /**
+   * Opens a webpage by given URL query path. After entering saving
+   * An HTML file of a page.
+   * @param path URL address where to go
+   * @param topic topic's title
+   */
+  async crawling(path, topic) {
+    await this.open(path); 
+    const html = await this.page.content();
+    fs.writeFile(`${this.location}/${topic}.html`, html, (err) => {
+      if (err) throw err;
+    });
+    
+  };
+};
+
+async function run() {
+  const techCrunchCrawler = new TechCrunchCrawler();
+  await techCrunchCrawler.launch();
+  await techCrunchCrawler.iterating();
+}
+
+run();
